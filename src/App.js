e60ea6ac1a3ec9ea0311b556aa47e71f792dd468
Jacqueline <proyectos@branta.com.co>
2021-06-15 20:51:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Mi primera aplicacion</h1>
+      <h1 className="App-title">Mi primera app React</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          todo lo que puedo hacer con react
-        </p>
+        <p className="App-intro">
+To get started, edit <code>src/App.js</code> and save to reload
+</p>
         <a
           className="App-link"
           href="https://reactjs.org"
